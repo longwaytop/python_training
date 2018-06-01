8eb07d45f5a2b17652f15f2e29c5697412ce0495
Alexander Isai <aliaksandr.isai@gmail.com>
2018-06-02 02:20:46 +0300

    small fixes

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -12,12 +12,12 @@ def app(request):
 
 
 def test_add_group(app):
-        app.session.login("admin", "secret")
+        app.session.login(username="admin", password="secret")
         app.group.create(Group(name="group_name", header="header", footer="footer"))
         app.session.logout()
 
 
 def test_empty_group(app):
-        app.session.login("admin", "secret")
+        app.session.login(username="admin", password="secret")
         app.group.create(Group(name="", header="", footer=""))
         app.session.logout()
