717ae6ee371b3830c1c055ccac2ef8a9f5e27407
longwaytop <aliaksandr.isai@gmail.com>
2018-06-01 12:53:42 +0300

    - recorded new test - improve code readability - transfer parameters to methods - extract methods and classes

---
+++ contact.py
@@ -0,0 +1,14 @@
+class Contact:
+    def __init__(self, first_name, last_name, address,
+                 home_phone, mobile_phone, work_phone,
+                 fax, first_email, second_email, third_email):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.address = address
+        self.home_phone = home_phone
+        self.mobile_phone = mobile_phone
+        self.work_phone = work_phone
+        self.fax = fax
+        self.first_email = first_email
+        self.second_email = second_email
+        self.third_email = third_email
