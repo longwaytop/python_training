0dcec379c3cb10205471575bfab444e2da3c88ba
longwaytop <aliaksandr.isai@gmail.com>
2018-06-04 18:38:59 +0300

    'modify group' test has been generated

--- fixture/group.py
+++ fixture/group.py
@@ -37,3 +37,27 @@ class GroupHelper:
         # submit deletion
         wd.find_element_by_name("delete").click()
         self.return_to_groups_page()
+
+    def modify_first_group(self):
+        wd = self.app.wd
+        self.open_groups_page()
+        # select first group
+        wd.find_element_by_name("selected[]").click()
+        # enter group editor
+        wd.find_element_by_name("edit").click()
+        # edit group form
+        wd.find_element_by_name("group_name").click()
+        wd.find_element_by_name("group_name").clear()
+        wd.find_element_by_name("group_name").send_keys("EDITED NAME")
+        wd.find_element_by_name("group_header").click()
+        wd.find_element_by_name("group_header").clear()
+        wd.find_element_by_name("group_header").send_keys("EDITED HEADER")
+        wd.find_element_by_name("group_footer").click()
+        wd.find_element_by_name("group_footer").clear()
+        wd.find_element_by_name("group_footer").send_keys("EDITED FOOTER")
+        # submit group edition
+        wd.find_element_by_name("update").click()
+        self.return_to_groups_page()
+
+
+
