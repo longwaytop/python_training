d824fe58b4e647f1631fb6c1af8d294a2fc7e416
longwaytop <aliaksandr.isai@gmail.com>
2018-06-05 19:55:32 +0300

    broken commit.

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -3,12 +3,10 @@ from model.group import Group
 
 
 def test_add_group(app):
-        app.session.login(username="admin", password="secret")
         app.group.create(Group(name="group_name", header="header", footer="footer"))
         app.session.logout()
 
 
 def test_empty_group(app):
-        app.session.login(username="admin", password="secret")
         app.group.create(Group(name="", header="", footer=""))
         app.session.logout()
