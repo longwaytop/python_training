40fcd96c4ad9e97463afd3b51adea3accad27a46
longwaytop <aliaksandr.isai@gmail.com>
2018-06-07 19:39:06 +0300

    intellectual login/logout improvement

--- fixture/contact.py
+++ fixture/contact.py
@@ -45,7 +45,7 @@ class ContactHelper:
         # select first contact
         wd.find_element_by_name("selected[]").click()
         # submit deletion
-        wd.find_element_by_xpath("// div[ @ id = 'content'] / form[2] / div[2] / input").click()
+        wd.find_element_by_xpath("//div[@id='content']/form[2]/div[2]/input").click()
         wd.switch_to_alert().accept()
 
     def modify_first_contact(self, contact):
--- test/test_add_contact.py
+++ test/test_add_contact.py
@@ -3,20 +3,16 @@ from model.contact import Contact
 
 
 def test_add_contact(app):
-        app.session.login(username="admin", password="secret")
         app.contact.create_contact(Contact(first_name="Andrey", last_name="Andreev", address="Minsk city",
                             home_phone="+375170000000", mobile_phone="+375255554433", work_phone="+375170011122",
                             fax="none", first_email="andrey_andreev@gmail.com", second_email="lullil@gmail.com",
                             third_email="bzzz@mail.ru"))
         app.return_to_home_page()
-        app.session.logout()
 
 
 def test_add_empty_contact(app):
-        app.session.login(username="admin", password="secret")
         app.contact.create_contact(Contact(first_name="", last_name="", address="",
                             home_phone="", mobile_phone="", work_phone="",
                             fax="", first_email="", second_email="",
                             third_email=""))
         app.return_to_home_page()
-        app.session.logout()
