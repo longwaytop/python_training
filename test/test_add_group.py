7633858928911aac86a77b0bfbee867e90e939b8
longwaytop <aliaksandr.isai@gmail.com>
2018-06-08 17:59:11 +0300

    check of the current session state

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -5,6 +5,5 @@ from model.group import Group
 def test_add_group(app):
         app.group.create(Group(name="group_name", header="header", footer="footer"))
 
-
 def test_empty_group(app):
         app.group.create(Group(name="", header="", footer=""))
