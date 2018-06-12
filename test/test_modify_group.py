ff579f749795af6ba2906093c4767b9808f6d7d1
Alexander Isai <aliaksandr.isai@gmail.com>
2018-06-13 00:41:34 +0300

    preconditions for test_modify_group

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -2,8 +2,12 @@ from model.group import Group
 
 
 def test_modify_group_name(app):
-    app.group.modify_first_group(Group(name="New group"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="tested_name"))
+    app.group.modify_first_group(Group(name="modified_group_name"))
 
 
 def test_modify_group_header(app):
-    app.group.modify_first_group(Group(header="New header"))
+    if app.group.count() == 0:
+        app.group.create(Group(name="tested_name"))
+    app.group.modify_first_group(Group(header="modified_group_header"))
