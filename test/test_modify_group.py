b5d230d674fb1ccf544da02657b07bcb06efd451
Alexander Isai <aliaksandr.isai@gmail.com>
2018-06-13 00:43:36 +0300

    fixed parameter description

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -3,11 +3,11 @@ from model.group import Group
 
 def test_modify_group_name(app):
     if app.group.count() == 0:
-        app.group.create(Group(name="tested_name"))
+        app.group.create(Group(name="tested_group"))
     app.group.modify_first_group(Group(name="modified_group_name"))
 
 
 def test_modify_group_header(app):
     if app.group.count() == 0:
-        app.group.create(Group(name="tested_name"))
+        app.group.create(Group(name="tested_group"))
     app.group.modify_first_group(Group(header="modified_group_header"))
