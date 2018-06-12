622d6ce5cb6ee000282c632578ef28691e86be2c
longwaytop <aliaksandr.isai@gmail.com>
2018-06-12 17:03:07 +0300

    fixed name of test_method

--- test/test_modify_group.py
+++ test/test_modify_group.py
@@ -5,5 +5,5 @@ def test_modify_group_name(app):
     app.group.modify_first_group(Group(name="New group"))
 
 
-def test_modify_group_footer(app):
+def test_modify_group_header(app):
     app.group.modify_first_group(Group(header="New header"))
