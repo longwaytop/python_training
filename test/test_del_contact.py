31b76356bfed454f61d9e2468078146c676c3085
Alexander Isai <aliaksandr.isai@gmail.com>
2018-06-20 17:57:45 +0300

    FAILED: delete_contact test: comparison of lists(old_contacts\new_contacts)

--- test/test_del_contact.py
+++ test/test_del_contact.py
@@ -5,4 +5,8 @@ def test_delete_first_contact(app):
     app.open_home_page()
     if app.contact.count() == 0:
         app.contact.create_contact(Contact(first_name="Alex"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.delete_first_contact()
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) - 1 == len(new_contacts)
+
