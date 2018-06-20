f4676bbe749ece21d2b8642f4b7c80a86158fce3
Alexander Isai <aliaksandr.isai@gmail.com>
2018-06-20 17:43:50 +0300

    modify_contact test: comparison of lists(old_contacts\new_contacts)

--- test/test_modify_contact.py
+++ test/test_modify_contact.py
@@ -5,11 +5,18 @@ def test_modify_contact_name(app):
     app.open_home_page()
     if app.contact.count() == 0:
         app.contact.create_contact(Contact(first_name="tested_contact_name"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(first_name="modified_first_name"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
 
 
 def test_modify_contact_address(app):
     app.open_home_page()
     if app.contact.count() == 0:
         app.contact.create_contact(Contact(first_name="tested_contact_name"))
+    old_contacts = app.contact.get_contact_list()
     app.contact.modify_first_contact(Contact(address="modified_address"))
+    new_contacts = app.contact.get_contact_list()
+    assert len(old_contacts) == len(new_contacts)
+
