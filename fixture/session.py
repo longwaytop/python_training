69129501287a1e039d9e1ff0a5d422c430ea647c
longwaytop <aliaksandr.isai@gmail.com>
2018-06-25 20:23:58 +0300

    locator improvement: replace xpath by css_selector

--- fixture/session.py
+++ fixture/session.py
@@ -12,7 +12,7 @@ class SessionHelper:
         wd.find_element_by_name("pass").click()
         wd.find_element_by_name("pass").clear()
         wd.find_element_by_name("pass").send_keys(password)
-        wd.find_element_by_xpath("//form[@id='LoginForm']/input[3]").click()
+        wd.find_element_by_css_selector('input[type="submit"]').click()
 
     def logout(self):
         wd = self.app.wd
