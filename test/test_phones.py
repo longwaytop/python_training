207304dbce51f9cb72df0e871d393762ca44fb24
longwaytop <aliaksandr.isai@gmail.com>
2018-06-27 11:55:30 +0300

    low fixes

--- test/test_phones.py
+++ test/test_phones.py
@@ -1,7 +1,7 @@
 def test_phones_on_home_page(app):
     contact_from_home_page = app.contact.get_contact_list()[0]
-    contact_from_edit_page = app.contact.get_contact_info_from_edit_page()[0]
-    assert contact_from_home_page.homephone == contact_from_edit_page.homephone
-    assert contact_from_home_page.workphone == contact_from_edit_page.workphone
-    assert contact_from_home_page.mobilephone == contact_from_edit_page.mobilephone
+    contact_from_edit_page = app.contact.get_contact_info_from_edit_page(0)
+    assert contact_from_home_page.home_phone == contact_from_edit_page.home_phone
+    assert contact_from_home_page.work_phone == contact_from_edit_page.work_phone
+    assert contact_from_home_page.mobile_phone == contact_from_edit_page.mobile_phone
     assert contact_from_home_page.fax == contact_from_edit_page.fax
