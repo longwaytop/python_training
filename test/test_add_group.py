acbb7735b3a2eebb48702c28511e6f42e782096f
longwaytop <aliaksandr.isai@gmail.com>
2018-07-04 18:33:00 +0300

    low fixes

--- test/test_add_group.py
+++ test/test_add_group.py
@@ -6,12 +6,12 @@ import string
 
 
 def random_string(prefix, maxlen):
-        symbols = string.ascii_letters + string.digits + string.punctuation + " "
+        symbols = string.ascii_letters + string.digits + string.punctuation + " "*10
         return prefix + "".join([random.choice(symbols) for i in range(random.randrange(maxlen))])
 
 
 testdata = [Group(name="", header="", footer="")] + [
-        Group(name=random_string("name", 10), header=random_string("header", 20), footer=random_string("footer", 20))
+        Group(name=random_string("name", 10), header=random_string("header", 10), footer=random_string("footer", 10))
         for i in range(5)
 ]
 
