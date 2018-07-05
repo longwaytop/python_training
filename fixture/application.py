2dfcd1e5306fee3ca322919d3ef328e1ffd8c831
longwaytop <aliaksandr.isai@gmail.com>
2018-07-05 15:51:43 +0300

    command line fix

--- fixture/application.py
+++ fixture/application.py
@@ -5,7 +5,7 @@ from fixture.contact import ContactHelper
 
 
 class Application:
-    def __init__(self, browser="firefox"):
+    def __init__(self, browser, base_url):
         if browser == "firefox":
             self.wd = webdriver.Firefox()
         elif browser == "chrome":
@@ -14,15 +14,15 @@ class Application:
             self.wd = webdriver.Ie()
         else:
             raise ValueError("Unrecognized browser %s" % browser)
-        self.wd = webdriver.Firefox(capabilities={"marionette": False})
         self.session = SessionHelper(self)
         self.group = GroupHelper(self)
         self.contact = ContactHelper(self)
+        self.base_url = base_url
 
     def open_home_page(self):
         wd = self.wd
         if not (wd.current_url.endswith("http://localhost/addressbook/") and len(wd.find_elements_by_link_text("add new")) > 0):
-            wd.get("http://localhost/addressbook/")
+            wd.get(self.base_url)
 
     def destroy(self):
         self.wd.quit()
